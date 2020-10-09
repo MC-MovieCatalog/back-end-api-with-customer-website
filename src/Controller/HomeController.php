872c74ae24960f66bfe7c5d0de959deb73413e1b
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2020-10-09 16:19:11 +0200

    home page integrated with assets

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('movie_book/home_page.html.twig', [
+            // 'controller_name' => 'HomePageController',
+        ]);
+    }
+}
