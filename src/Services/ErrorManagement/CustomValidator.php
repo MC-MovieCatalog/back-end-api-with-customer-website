b685470bfdb9f15f2f4c73ac141afe8065ebaaad
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2021-02-05 16:21:55 +0100

    Errors management

---
+++ src/Services/ErrorManagement/CustomValidator.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Services\ErrorManagement;
+
+use App\Services\ErrorManagement\ErrorAssert;
+use App\Controller\API\APIDefaultController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * This class handles errors for the entities controllers
+ */
+class CustomValidator extends ErrorAssert
+{
+    protected function stringFieldValidator($request, $field, ?int $minLength, ?int $maxLength)
+    {
+
+        $fieldErrors = [];
+
+        // if not integer value ?
+        if ($this->errorStringAssert($request, $field)) {
+            array_push($fieldErrors, $this->errorStringAssert($request, $field));
+        }
+        // if < min lenght ?
+        if ($minLength !== null) {
+            if ($this->errorStringMinLenghtAssert($request, $field, $minLength)) {
+                array_push($fieldErrors, $this->errorStringMinLenghtAssert($request, $field, $minLength));
+            }
+        }
+        // if > max lenght ?
+        if ($maxLength !== null) {
+            if ($this->errorStringMaxLenghtAssert($request, $field, $maxLength)) {
+                array_push($fieldErrors, $this->errorStringMaxLenghtAssert($request, $field, $maxLength));
+            }
+        }
+
+        return $fieldErrors;
+    }
+
+    protected function integerFieldValidator($request, $field, ?int $minLength, ?int $maxLength, ?int $minValue, ?int $maxValue)
+    {
+
+        $fieldErrors = [];
+
+        // if not integer value ?
+        if ($this->errorIntegerAssert($request, $field)) {
+            array_push($fieldErrors, $this->errorIntegerAssert($request, $field));
+        }
+        // if < min lenght ?
+        if ($minLength !== null) {
+            if ($this->errorIntegerMinLenghtAssert($request, $field, $minLength)) {
+                array_push($fieldErrors, $this->errorIntegerMinLenghtAssert($request, $field, $minLength));
+            }
+        }
+        // if > max lenght ?
+        if ($maxLength !== null) {
+            if ($this->errorIntegerMaxLenghtAssert($request, $field, $maxLength)) {
+                array_push($fieldErrors, $this->errorIntegerMaxLenghtAssert($request, $field, $maxLength));
+            }
+        }
+        // if < min value ?
+        if ($minValue !== null) {
+            if ($this->errorIntegerMinValeuAssert($request, $field, $minValue)) {
+                array_push($fieldErrors, $this->errorIntegerMinValeuAssert($request, $field, $minValue));
+            }
+        }
+        // if > max value ?
+        if ($maxValue !== null) {
+            if ($this->errorIntegerMaxValueAssert($request, $field, $maxValue)) {
+                array_push($fieldErrors, $this->errorIntegerMaxValueAssert($request, $field, $maxValue));
+            }
+        }
+
+        return $fieldErrors;
+    }
+
+    protected function customStringValidator($request, $field, ?int $minLength, ?int $maxLength)
+    {
+        $stringErrors = $this->stringFieldValidator($request, $field, $minLength, $maxLength);
+
+        // if not integer value ?
+        if ($this->errorStringAssert($request, $field)) {
+            array_push($stringErrors, $this->errorStringAssert($request, $field));
+        }
+
+        return $stringErrors;
+    }
+
+    protected function customUrlValidator($request, $field, ?int $minLength, ?int $maxLength)
+    {
+        $stringErrors = $this->customStringValidator($request, $field, $minLength, $maxLength);
+
+        // if not integer value ?
+        if ($this->errorUrlAssert($request, $field)) {
+            array_push($stringErrors, $this->errorUrlAssert($request, $field));
+        }
+
+        return $stringErrors;
+    }
+
+    protected function customIntegerValidator($request, $field, ?int $minLength, ?int $maxLength, ?int $minValue, ?int $maxValue)
+    {
+        $integerErrors = $this->integerFieldValidator($request, $field, $minLength, $maxLength, $minValue, $maxValue);
+
+        // if not integer value ?
+        if ($this->errorIntegerAssert($request, $field)) {
+            array_push($integerErrors, $this->errorIntegerAssert($request, $field));
+        }
+
+        return $integerErrors;
+    }
+
+    protected function customFloatValidator($request, $field)
+    {
+        $floatErrors = [];
+        // if not float/double value ?
+        if ($this->errorFloatAssert($request, $field)) {
+            array_push($floatErrors, $this->errorFloatAssert($request, $field));
+        }
+
+        return $floatErrors;
+    }
+
+}
---
+++ src/Services/ErrorManagement/ErrorAssert.php
@@ -0,0 +1,231 @@
+<?php
+
+namespace App\Services\ErrorManagement;
+
+/**
+ * This class handles errors for the API responses
+ */
+class ErrorAssert
+{
+    // Empty data
+
+    /**
+     * This function checks whether the content of the specified field is defined.
+     *
+     * @param array $data
+     * @param string $field
+     * @param string $message
+     * @return array
+     */
+    protected function errorEmptyAssert(array $data, string $field, string $message = 'Ce champ est obligatoire')
+    {
+        if (array_key_exists($field, $data) === false) {
+            return ['field' => $field, 'message' => $message];
+        }
+        
+        // return array_key_exists($field, $data) ? true : ['field' => $field, 'message' => $message];
+    }
+
+    /**
+     * This function checks whether the content of the specified value is null.
+     *
+     * @param array $data
+     * @param string $field
+     * @param string $message
+     */
+    protected function errorDefinedAssert(array $data, string $field)
+    {
+        if (array_key_exists($field, $data)) {
+            return true;
+        }
+    }
+
+    // Integer 
+
+    /**
+     * This function checks if the indicated value is of type integer
+     *
+     * @param array $data
+     * @param string $field
+     * @param string $message
+     * @return array
+     */
+    protected function errorIntegerAssert(array $data, string $field, string $message = 'Cette valeur doit être un nombre entier')
+    {
+        if (is_int($data[$field]) !== true) {
+            return ['field' => $field, 'message' => $message];
+        }
+    }
+
+    /**
+     * This function checks the minimum size of the entered int value
+     *
+     * @param array $data
+     * @param string $field
+     * @param integer $minLenght
+     * @param string $message
+     * @return array
+     */
+    protected function errorIntegerMinLenghtAssert(
+        array $data,
+        string $field,
+        int $minLenght,
+        string $message = 'Le nombre saisit ne peut peut contenir moins de '
+    ) {
+        $error = ($message !== 'Le nombre saisit ne peut peut contenir moins de ') ? $message : $message . strval($minLenght) . ' chiffres(s)';
+
+        if (is_int($data[$field]) === true && strlen((string)$data[$field]) < $minLenght) {
+            return ['field' => $field, 'error' => $error];
+        }
+    }
+
+    /**
+     * This function checks the maximum size of the entered int value
+     *
+     * @param array $data
+     * @param string $field
+     * @param integer $maxLenght
+     * @param string $message
+     * @return array
+     */
+    protected function errorIntegerMaxLenghtAssert(
+        array $data,
+        string $field,
+        int $maxLenght,
+        string $message = 'Le nombre saisit ne peut peut contenir plus de '
+    ) {
+        $error = ($message !== 'Le nombre saisit ne peut peut contenir plus de ') ? $message : $message . strval($maxLenght) . ' chiffre(s)';
+
+        if (is_int($data[$field]) === true && strlen((string)$data[$field]) > $maxLenght) {
+            return ['field' => $field, 'error' => $error];
+        }
+    }
+
+    /**
+     * This function checks the minimum int value entered
+     *
+     * @param array $data
+     * @param string $field
+     * @param integer $minValue
+     * @param string $message
+     * @return array
+     */
+    protected function errorIntegerMinValeuAssert(
+        array $data,
+        string $field,
+        int $minValue,
+        string $message = 'Le nombre saisit ne peut pas être plus petit que '
+    ) {
+        $error = ($message !== 'Le nombre saisit ne peut pas être plus petit que ') ? $message : $message . strval($minValue);
+
+        if (is_int($data[$field]) === true && $data[$field] < $minValue) {
+            return ['field' => $field, 'error' => $error];
+        }
+    }
+
+    /**
+     * This function checks the maximum int value entered | max value 2147483647
+     *
+     * @param array $data
+     * @param string $field
+     * @param integer $maxValue
+     * @param string $message
+     * @return array
+     */
+    protected function errorIntegerMaxValueAssert(
+        array $data,
+        string $field,
+        int $maxValue,
+        string $message = 'La nombre saisit ne peut pas être plus grand que '
+    ) {
+        $error = ($message !== 'La nombre saisit ne peut pas être plus grand que ') ? $message : $message . strval($maxValue);
+
+        if (is_int($data[$field]) === true && $data[$field] > $maxValue) {
+            return ['field' => $field, 'error' => $error];
+        }
+    }
+
+    // String
+
+    /**
+     * This function checks if the indicated value is of type string
+     *
+     * @param array $data
+     * @param string $field
+     * @param string $message
+     * @return array
+     */
+    protected function errorStringAssert(array $data, string $field, string $message = 'Cette valeur doit être du texte')
+    {
+        if (is_string($data[$field]) === false) {
+            return ['field' => $field, 'message' => $message];
+        }
+    }
+
+
+    /**
+     * This function checks the minimum size of the entered string value
+     *
+     * @param array $data
+     * @param string $field
+     * @param integer $minLenght
+     * @param string $message
+     * @return array
+     */
+    protected function errorStringMinLenghtAssert(
+        array $data,
+        string $field,
+        int $minLenght,
+        string $message = 'Vous ne pouvez pas saisir moins de '
+    ) {
+        $error = ($message !== 'Vous ne pouvez pas saisir moins de ') ? $message : $message . strval($minLenght) . ' caractères';
+
+        if (is_string($data[$field]) === true && strlen((string)$data[$field]) < $minLenght) {
+            return ['field' => $field, 'error' => $error];
+        }
+    }
+
+    /**
+     * This function checks the minimum size of the entered string value
+     *
+     * @param array $data
+     * @param string $field
+     * @param integer $maxLenght
+     * @param string $message
+     * @return array
+     */
+    protected function errorStringMaxLenghtAssert(
+        array $data,
+        string $field,
+        int $maxLenght,
+        string $message = 'Vous ne pouvez pas saisir plus de '
+    ) {
+        $error = ($message !== 'Vous ne pouvez pas saisir plus de ') ? $message : $message . strval($maxLenght) . ' caractères';
+
+        if (is_string($data[$field]) === true && strlen((string)$data[$field]) > $maxLenght) {
+            return ['field' => $field, 'error' => $error];
+        }
+    }
+
+    /**
+     * This function checks whether the content of the specified value is not empty.
+     *
+     * @param array $data
+     * @param string $field
+     * @param string $message
+     * @return array
+     */
+    protected function errorFloatAssert(array $data, string $field, string $message = 'Cette valeur doit être un nombre décimal')
+    {
+        if (is_float($data[$field]) === false) {
+            return ['field' => $field, 'message' => $message];
+        }
+    }
+    // public function errorAsset($data, $field, $lenght = null, $message = null)
+    
+    protected function errorUrlAssert(array $data, string $field, string $message = 'Cette url n\'est pas valide') {
+        if (!filter_var($data[$field], FILTER_VALIDATE_URL)) {
+            return ['field' => $field, 'message' => $message];
+        }
+    }
+}
