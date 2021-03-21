8b5136aec46aa8827ab5a48cd6674bf70e44731a
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2021-03-21 18:30:36 +0100

    Change password form

---
+++ src/Form/ChangePasswordFormType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ChangePasswordFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Merci de bien vouloir entrer un mot de passe.',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe ne peut pas dépasser {{ limit }} caractères',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+                    'label' => false,
+                    'attr' => ['placeholder' => 'Nouveau mot de passe']
+                ],
+                'second_options' => [
+                    'label' => false,
+                    'attr' => ['placeholder' => 'Confirmation du mot de passe']
+                ],
+                'invalid_message' => 'Les deux mots de passe ne correspondent pas.',
+                // Instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
