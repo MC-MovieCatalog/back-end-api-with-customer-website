58dfd0376568c6fd06293c52b80574a51ddc2ae0
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2021-03-28 12:31:00 +0200

    Fonctionnalités déjà implémentées.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -26,34 +26,4 @@ class UserController extends AbstractController
             // 'controller_name' => 'HomePageController',
         ]);
     }
-
-    /**
-     * @Route("/inscription", name="sign_up")
-     */
-    public function sign_up()
-    {
-        return $this->render('movie_book/security/sign-up.html.twig', [
-            // 'controller_name' => 'HomePageController',
-        ]);
-    }
-
-    /**
-     * @Route("/connexion", name="login")
-     */
-    public function login()
-    {
-        return $this->render('movie_book/security/login.html.twig', [
-            // 'controller_name' => 'HomePageController',
-        ]);
-    }
-
-    /**
-     * @Route("/mot-de-passe-oublie", name="forgot_password")
-     */
-    public function reset_password()
-    {
-        return $this->render('movie_book/security/forgot_password.html.twig', [
-            // 'controller_name' => 'HomePageController',
-        ]);
-    }
 }
