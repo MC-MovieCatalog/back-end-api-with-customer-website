91c347ecdde4381bd47c2e57e87767168a988bd3
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2021-04-11 18:10:13 +0200

    Ajout du jeu de données des invoices et des ratings

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,9 +6,11 @@ use Faker\Factory;
 use App\Entity\User;
 use App\Entity\Movie;
 use App\Entity\Address;
+use App\Entity\Rating;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\Invoice;
 
 class AppFixtures extends Fixture
 {
@@ -22,17 +24,16 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        // Common variables
-
-        $faker = Factory::create('fr_FR');
-
         // Data
 
         $fakeMovieData = new FakeMovieData();
         $fakeAdresseData = new FakeAdresseData();
         $fakeUserData = new FakeUserData();
+        $fakeInvoiceData = new FakeInvoiceData();
 
-        // Movie 
+        // Movie
+        $movies = [];
+
         $fakeMovies = $fakeMovieData->getMovies();
         
         foreach ($fakeMovies as $movieData) {
@@ -48,25 +49,13 @@ class AppFixtures extends Fixture
                  ->setTrailer($movieData['trailer']);
 
             $manager->persist($movie);
+            $movies[] = $movie;
             
         }
 
-        // Address
-        $fakeAddresses = $fakeAdresseData->getAddresses();
-
-        foreach ($fakeAddresses as $fakeAddress) {
-            $address = new Address();
-
-            $address->setStreetNb($fakeAddress['streetNb'])
-                    ->setAddress($fakeAddress['address'])
-                    ->setPostal($fakeAddress['postal'])
-                    ->setCity($fakeAddress['city'])
-                    ->setType($fakeAddress['type']);
-
-            $manager->persist($address);
-        }
-
         // User
+        $users = [];
+
         $fakeUsers = $fakeUserData->getUsers();
 
         foreach ($fakeUsers as $fakeUser) {
@@ -82,6 +71,71 @@ class AppFixtures extends Fixture
                     ->setAgreeTerms($fakeUser['agreeTerms']);
 
             $manager->persist($user);
+            $users[] = $user;
+        }
+
+        // Address
+        $addresses = [];
+        
+        $fakeAddresses = $fakeAdresseData->getAddresses();
+
+        foreach ($fakeAddresses as $fakeAddress) {
+
+            $user_address = $users[mt_rand(0, count($users) - 1)];
+
+            $address = new Address();
+
+            $address->setStreetNb($fakeAddress['streetNb'])
+                    ->setAddress($fakeAddress['address'])
+                    ->setPostal($fakeAddress['postal'])
+                    ->setCity($fakeAddress['city'])
+                    ->setType($fakeAddress['type'])
+                    ->setUser($user_address);
+
+            $manager->persist($address);
+
+            $addresses[] = $address;
+        }
+        
+        // Invoice
+        $fakeInvoices = $fakeInvoiceData->getInvoices();
+        
+        foreach ($fakeInvoices as $fakeInvoice) {
+
+            $invoice_movies = array_rand($movies, 3);
+
+            $invoice_address = $addresses[mt_rand(0, count($addresses) -1)];
+            
+            $invoice = new Invoice();
+            $fakeInvoice = $invoice;
+            $amount = 0;
+            
+            for ($i=0; $i < count($invoice_movies); $i++) { 
+                $fakeInvoice->addMovie($movies[$invoice_movies[$i]]);
+                $amount = $amount + $movies[$invoice_movies[$i]]->getPrice();
+            }
+
+            $fakeInvoice->setAmount($amount)
+                    ->setCustomer($invoice_address->getUser())
+                    ->setAddress($invoice_address);
+
+            $manager->persist($fakeInvoice);        
+        }
+        
+
+        // Rating
+        for ($r = 0; $r < 20; $r++) {
+
+            $author = $users[mt_rand(0, count($users) - 1)];
+            $movie = $movies[mt_rand(0, count($movies) - 1)];
+
+            $rating = new Rating();
+
+            $rating->setRating(mt_rand(1, 5))
+                ->setAuthor($author)
+                ->setMovie($movie);
+
+            $manager->persist($rating);
         }
 
         // Flush
